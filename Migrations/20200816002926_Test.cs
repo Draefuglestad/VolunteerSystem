e6a12247a45a592b1beddf959307c1fb1f748895
Katrina <n01444842@unf.edu>
2020-08-15 23:33:05 -0400

    Changes

---
+++ Migrations/20200816002926_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VolunteerSystem.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "volunteerDate",
+                table: "Opportunities",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "volunteerDate",
+                table: "Opportunities");
+        }
+    }
+}
