4361bb33e274a570b9263b92a62a0ee8a1addd25
Katrina <n01444842@unf.edu>
2020-08-17 16:08:00 -0400

    Opp Matching functionality

---
+++ Models/MixModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using VolunteerSystem.Models;
+using VolunteerSystem.Models.ViewModels;
+
+namespace VolunteerSystem.Models
+{
+    public class MixModel
+    {
+        public Volunteer Volunteer { get; set; }
+        public Opportunity Opportunity{ get; set; }
+    }
+}
